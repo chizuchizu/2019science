55f3bd0ccffe7fe658bd6c623dc644cb8f0ab6aa
chizuchizu <ochimasumi@gmail.com>
2019-07-03 17:57:33 +0900

    リサイズ

---
+++ preprocessing/resize.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import os
+import glob
+from PIL import Image
+
+"""
+画像のリサイズ(128x128にする)
+"""
+
+image_size = 128
+
+from_dir = "/home/yuma/デスクトップ/自由研究/"
+to_dir = "/home/yuma/PycharmProjects/2019science/data/resize"
+
+os.makedirs(to_dir, exist_ok=True)
+
+for path_ in glob.glob(os.path.join(from_dir + "/*")):
+    """親フォルダの読み込み"""
+    for path in glob.glob(os.path.join(path_ + "/*.JPG")):
+        """フォルダ内の画像の読み込み"""
+        img = Image.open(path)
+        img = img.resize((image_size, image_size))
+        folder = os.path.dirname(path)
+        folds = folder[len(os.path.dirname(folder)):]
+        basename = os.path.basename(path)
+        to_path = to_dir + folds
+        os.makedirs(to_path, exist_ok=True)
+        img.save(os.path.join(to_path, basename))
+        print(path)
+
+print("Done")
