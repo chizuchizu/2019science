acaaaf41d1b5d50b42f905a03ae30313aba15237
chizuchizu <ochimasumi@gmail.com>
2019-07-15 21:27:29 +0900

    Preprocess.pyをpreprocess.pyに変更

--- preprocessing/__init__.py
+++ preprocessing/__init__.py
@@ -1,3 +1,3 @@
 from preprocessing.mizumasi import Inflated as Ifl
 from preprocessing.count import image_count
-from preprocessing.Preprocess import Preprocess
+from preprocessing.preprocess import Preprocess
